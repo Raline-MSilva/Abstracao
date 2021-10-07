673cd5c60fbcbe22fe0eace7ca671f30b34b557b
Raline Silva <ralinesilva1@gmail.com>
2021-10-07 15:44:47 -0300

    Modificar classe consumidor

--- src/br/com/zup/Fatura/Consumidor.java
+++ src/br/com/zup/Fatura/Consumidor.java
@@ -3,14 +3,16 @@ package br.com.zup.Fatura;
 public class Consumidor {
     private String nome;
     private String email;
+    private TipoConsumidor consumidor;
 
     public Consumidor() {
 
     }
 
-    public Consumidor(String nome, String email) {
+    public Consumidor(String nome, String email, TipoConsumidor consumidor) {
         this.nome = nome;
         this.email = email;
+        this.consumidor = consumidor;
     }
 
     public String getNome() {
@@ -29,11 +31,20 @@ public class Consumidor {
         this.email = email;
     }
 
+    public TipoConsumidor getConsumidor() {
+        return consumidor;
+    }
+
+    public void setConsumidor(TipoConsumidor consumidor) {
+        this.consumidor = consumidor;
+    }
+
     @Override
     public String toString() {
         StringBuilder retorno = new StringBuilder();
         retorno.append("\n Nome: " + nome);
         retorno.append("\n Email: " + email);
+        retorno.append("\n Tipo do Consumidor: " + consumidor);
         return retorno.toString();
     }
 }
--- src/br/com/zup/Fatura/Main.java
+++ src/br/com/zup/Fatura/Main.java
@@ -2,10 +2,10 @@ package br.com.zup.Fatura;
 
 public class Main {
     public static void main(String[] args) {
-        Consumidor consumidor = new Consumidor("Raline", "raline@raline");
-        Fatura fatura = new Fatura(consumidor, 900, "04/10/12" );
+        //Consumidor consumidor = new Consumidor("Raline", "raline@raline");
+        //Fatura fatura = new Fatura(consumidor, 900, "04/10/12" );
 
-        System.out.println(fatura);
-        System.out.println(consumidor);
+        //System.out.println(fatura);
+        //System.out.println(consumidor);
     }
 }
