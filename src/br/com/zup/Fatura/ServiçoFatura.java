d8fae11ac52c7730ddce861196fdc1217fef38d9
Raline Silva <ralinesilva1@gmail.com>
2021-10-07 15:39:37 -0300

    Criar enum consumidor

--- src/br/com/zup/Fatura/ServiçoFatura.java
+++
@@ -1,8 +0,0 @@
-package br.com.zup.Fatura;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class ServiçoFatura {
-    private static List<Fatura> faturas = new ArrayList<>();
-}
---
+++ src/br/com/zup/Fatura/TipoConsumidor.java
@@ -0,0 +1,6 @@
+package br.com.zup.Fatura;
+
+public enum TipoConsumidor {
+    PESSOA_FISICA,
+    PESSOA_JURIDICA
+}
