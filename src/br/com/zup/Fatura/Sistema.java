7a6aee9c98fdcb0c403dbd5086993aeba3941860
Raline Silva <ralinesilva1@gmail.com>
2021-10-07 15:59:52 -0300

    Criar menu

--- src/br/com/zup/Fatura/Sistema.java
+++ src/br/com/zup/Fatura/Sistema.java
@@ -10,4 +10,12 @@ public class Sistema {
         System.out.println(mensagem);
         return capturarDados;
     }
+
+    //criar menu
+    public static void menu (){
+        System.out.println("Bem vindo ao sistema de Faturas Você em Divída!");
+        System.out.println("Digite 1 para cadastrar um consumidor");
+        System.out.println("Digite 2 para cadastrar uma fatura");
+        System.out.println("Digite 3 para sair do sistema");
+    }
 }
