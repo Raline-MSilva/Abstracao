632d54c9b8b5f7de42d8a87ef312c27e9d1c9ee7
Raline Silva <ralinesilva1@gmail.com>
2021-10-07 16:35:13 -0300

    Criar lista consumidores

--- src/br/com/zup/Fatura/ServiçoConsumidor.java
+++ src/br/com/zup/Fatura/ServiçoConsumidor.java
@@ -1,4 +1,18 @@
 package br.com.zup.Fatura;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServiçoConsumidor {
+    private static List<Consumidor> consumidores = new ArrayList<>();
+
+    public static List<TipoConsumidor> mostrarTipos () {
+        List<TipoConsumidor> consumidores = new ArrayList<>();
+
+        //percorrer minha lista
+        for (TipoConsumidor tipoReferencia: TipoConsumidor.values()){
+            consumidores.add(tipoReferencia);
+        }
+        return consumidores;
+    }
 }
--- src/br/com/zup/Fatura/Sistema.java
+++ src/br/com/zup/Fatura/Sistema.java
@@ -1,10 +1,11 @@
 package br.com.zup.Fatura;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Sistema {
-    //Obter dados do usuário
 
+    //Obter dados do usuário
     private static Scanner capturarDados(String mensagem) {
         Scanner capturarDados = new Scanner(System.in);
         System.out.println(mensagem);
@@ -17,5 +18,15 @@ public class Sistema {
         System.out.println("Digite 1 para cadastrar um consumidor");
         System.out.println("Digite 2 para cadastrar uma fatura");
         System.out.println("Digite 3 para sair do sistema");
+    }
+
+    //recebendo informações do consumidor
+    public static void cadastrarConsumidor (){
+        String nome = capturarDados("Informe o nome do consumidor").nextLine();
+        String email = capturarDados("Informe o email do consumidor").nextLine();
+    }
+    //percorrer lista para mostrar o tipo de consumidor
+    public static void menuTipoConsumidor (){
+        List<TipoConsumidor> consumidores = ServiçoConsumidor.mostrarTipos();
     }
 }
