4cc02fcc2430d5ce5cd9e63413daea32704e63e5
Raline Silva <ralinesilva1@gmail.com>
2021-10-07 08:48:58 -0300

    Criar classe ServiçoConsumidor

---
+++ src/br/com/zup/Fatura/ServiçoConsumidor.java
@@ -0,0 +1,10 @@
+package br.com.zup.Fatura;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiçoConsumidor {
+    // privado e estatico pois não vamos instanciar um ServiçoConsumidor
+    private static List<Consumidor> consumidores = new ArrayList<>();
+
+}
