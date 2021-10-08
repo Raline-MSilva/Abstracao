4a74cf798ac83d6d3c1d156b6a3933360e97fed1
Raline Silva <ralinesilva1@gmail.com>
2021-10-08 09:38:02 -0300

    Entrega minima

--- src/br/com/zup/Fatura/Main.java
+++ src/br/com/zup/Fatura/Main.java
@@ -3,7 +3,9 @@ package br.com.zup.Fatura;
 public class Main {
     public static void main(String[] args) {
         try {
+
             Sistema.cadastrarConsumidor();
+            Sistema.cadastrarFatura();
             ServiçoConsumidor.listarConsumidores();
 
         }catch (Exception deuErro){
